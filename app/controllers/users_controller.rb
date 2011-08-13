4b237fed858e934d209a114bd01cf1cb760fac2d
HPNeo <hpneo@hotmail.com>
2011-08-13 12:31:37 -0500

    users#show ya filtra por fechas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if params[:date]
-      @categories = Task.where("created_at >= :date OR created_at <= :date", { :date => params[:date] }).map{|i| i.category }.uniq
+      @categories = Task.where("DATE(created_at) = #{params[:date]}").map{|i| i.category }.uniq
     else
       @categories = @user.categories
     end
